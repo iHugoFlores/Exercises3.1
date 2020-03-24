04bfdd54675305ee43c4e2374ecf443d5aeb1fcb
Hugo Flores <hfp.ing2@gmail.com>
2020-03-24 11:41:54 -0400

    Objective C project created

---
+++ ImagesAndButtonsObjC/ImagesAndButtonsObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagesAndButtonsObjC
+//
+//  Created by Field Employee on 3/24/20.
+//  Copyright © 2020 Hugo Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImagesAndButtonsObjC/ImagesAndButtonsObjC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ImagesAndButtonsObjC
+//
+//  Created by Field Employee on 3/24/20.
+//  Copyright © 2020 Hugo Flores. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
